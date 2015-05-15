adbb458c3a837885fceeaecd595eb1e3fd469882
Arthur Pachachura <pachachura.arthur@gmail.com>
2015-05-15 16:03:21 -0500

    Make IP Form

---
+++ BigIP.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace BigIP
+{
+    partial class BigIP
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Address = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Address
+            // 
+            this.Address.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.Address.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Address.Location = new System.Drawing.Point(12, 9);
+            this.Address.Name = "Address";
+            this.Address.Size = new System.Drawing.Size(376, 82);
+            this.Address.TabIndex = 0;
+            this.Address.Text = "Getting IP...";
+            this.Address.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // BigIP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(400, 100);
+            this.ControlBox = false;
+            this.Controls.Add(this.Address);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "BigIP";
+            this.Opacity = 0.7D;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Big IP";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Address;
+    }
+}
+
---
+++ BigIP.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BigIP
+{
+    public partial class BigIP : Form
+    {
+        public BigIP()
+        {
+            InitializeComponent();
+        }
+    }
+}
